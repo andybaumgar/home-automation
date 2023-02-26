e82f988e1d9e55534e0f9cb7b3a20937cf165745
andybaumgar <andybaumgar@gmail.com>
2023-02-26 15:14:55 -0500

    Update start.sh

--- air-gradient-agent/start.sh
+++ air-gradient-agent/start.sh
@@ -1,2 +1,4 @@
+docker stop air-gradient-agent
+docker rm air-gradient-agent
 docker build -t air-gradient-agent .
-docker run -d --name air-gradient-agent -p 9926:9926 --rm --privileged --network=host --restart unless-stopped air-gradient-agent
+docker run -d --name air-gradient-agent -p 9926:9926 --privileged --network=host --restart unless-stopped air-gradient-agent
